74ae67f28fb4bb4c197843328bba42166e096b69
rekiraly@gmail.com <rekiraly@gmail.com>
2026-06-23 23:15:36 +0200

    daten und schema

---
+++ uebungsblatt_1/DATEN_U2.sql
@@ -0,0 +1,44 @@
+INSERT INTO Abteilung VALUES
+('Personal'),
+('Vertrieb'),
+('Marketing'),
+('Buchhaltung'),
+('IT'),
+('Entwicklung'),
+('Produktion'),
+('Einkauf'),
+('Logistik'),
+('Verwaltung');
+
+INSERT INTO Arbeitszeitmodell VALUES
+
+('ho', 'HomeOffice'),
+('tz', 'Teilzeit'),
+('vz', 'Vollzeit'),
+('mm', 'Minijob');
+
+INSERT INTO Familienstand VALUES
+
+('ledig'),
+('verheiratet'),
+('geschieden'),
+('verwitwet'),
+('eingetr. Lebensgemeinschaft');
+
+INSERT INTO Mitarbeiter VALUES
+
+('Fahrer', 'Bob', 'Mozartstr. 7', '90768',	'Fuerth', '1972-11-02', 5, 7, 'vz'), 
+('Platz', 'Alexander', 'Lessingstr. 72', '91054', 'Erlangen', '1955-08-01', 2, 7, 'vz'), 
+('Kreuz', 'Andreas', 'Ringstr. 40', '90765','Fuerth', '1974-04-26', 3, 2, 'ho'), 
+('Geber', 'Ann', 'Schillerstr. 25', '90762','Fuerth', '1987-03-17', 3, 10, 'vz'), 
+('Zug ',  'Ann', 'Lindenweg 12', '90411',	'Nuernberg', '1996-03-20', 5, 2, 'mm'), 
+('Bolika','Anna', 'Bahnhofstr. 142', '90425','Nuernberg', '1981-04-20', 5, 6, 'vz'), 
+('Nass',  'Anna', 'Dorfstr. 18', '90409',	'Nuernberg', '1959-02-10', 2, 5, 'ho'), 
+('Theke', 'Anna', 'Kirchplatz 60', '91052',	'Erlangen', '1964-01-23', 4, 5, 'tz'), 
+('Höhle', 'Axel', 'Mühlgasse 94', '90766',	'Fuerth', '1969-08-04', 3, 6, 'vz'), 
+('Diener','Bernhard', 'Mühlgasse 69', '90763','Fuerth', '1961-05-28', 5, 3, 'vz'); 
+
+--SELECT * FROM Familienstand 
+--SELECT * FROM Arbeitszeitmodell 
+--SELECT * FROM Abteilung 
+--SELECT * FROM Mitarbeiter 
--- uebungsblatt_1/schema.sql
+++ uebungsblatt_1/schema.sql
@@ -1,38 +1,69 @@
---DROP TABLE mitarbeiter;
---DROP TABLE abteilung;
---DROP TABLE arbeitszeitmodell;
---DROP TABLE familienstand;
+ --DROP TABLE Mitarbeiter;
+ --DROP TABLE Abteilung;
+ --DROP TABLE Arbeitszeitmodell;
+ --DROP TABLE Familienstand;
 
-CREATE TABLE abteilung (
-abtnr INT IDENTITY(1,1) PRIMARY KEY,
-abtbez VARCHAR(100)
+CREATE TABLE Familienstand
+(f_id INT IDENTITY(1,1) PRIMARY KEY,
+fam_bez VARCHAR(50),
 );
 
-CREATE TABLE arbeitszeitmodell (
-modell_code INT IDENTITY(1,1) PRIMARY KEY,
-az_bez VARCHAR(100)
+--INSERT INTO Familienstand (fam_bez) VALUES
+
+--('ledig'),
+--('verheiratet'),
+--('geschieden'),
+--('verwitwet'),
+--('eingetr. Lebensgemeinschaft');
+
+--SELECT * FROM Familienstand;
+
+
+CREATE TABLE Arbeitszeitmodell
+(azm_id CHAR(2) PRIMARY KEY,
+az_bez VARCHAR(20),
 );
 
-CREATE TABLE familienstand (
-f_id INT IDENTITY(1,1) PRIMARY KEY,
-fam_bez VARCHAR(100)
+CREATE TABLE Abteilung
+(abt_id INT IDENTITY(1,1) PRIMARY KEY,
+abtbez VARCHAR(20),
 );
 
-CREATE TABLE mitarbeiter (
-ma_id INT IDENTITY(1,1),
-nname VARCHAR(100),
+--INSERT INTO Abteilung (abtbez) VALUES
+--('Personal'),
+--('Vertrieb'),
+--('Marketing'),
+--('Buchhaltung'),
+--('IT'),
+--('Entwicklung'),
+--('Produktion'),
+--('Einkauf'),
+--('Logistik'),
+--('Verwaltung');
+
+SELECT * FROM Abteilung;
+
+
+CREATE TABLE Mitarbeiter
+(ma_id INT IDENTITY(1,1),
 vorname VARCHAR(100),
-str_hnr VARCHAR (100),
-plz VARCHAR (5),
+nachname VARCHAR(100),
+str_hsnr VARCHAR(100),
+plz CHAR(5),
 ort VARCHAR(50),
-gebdatum DATe,
+geburtsdatum DATE,
 f_id INT,
+abt_id INT,
 azm_id CHAR(2),
-abt_nr INT,
-CONSTRAINT PK_mitarbeiter  PRIMARY KEY(ma_id),
-CONSTRAINT FK_ma_fam_f_id FOREIGN KEY (f_id) REFERENCES familienstand(F_id),
-CONSTRAINT FK_ma_azm_azm_id FOREIGN KEY (f_id) REFERENCES arbeitszeitmodell(modell_code),
-CONSTRAINT FK_ma_abt_abt_nr FOREIGN KEY (f_id) REFERENCES abteilung(abtnr)
+CONSTRAINT PK_mitarbeiter PRIMARY KEY (ma_id),
+CONSTRAINT FK_ma_fam_f_id FOREIGN KEY (f_id) REFERENCES familienstand(f_id),
+CONSTRAINT FK_ma_azm_azm_id FOREIGN KEY (azm_id) REFERENCES arbeitszeitmodell(azm_id),
+CONSTRAINT FK_ma_abt_abt_id FOREIGN KEY (abt_id) REFERENCES abteilung(abt_id)
+--FOREIGN KEY (f_id) REFERENCES Familienstand(f_id),
+--FOREIGN KEY (azm_id) REFERENCES Arbeitszeitmodell(azm_id),
+--FOREIGN KEY (abt_id) REFERENCES Abteilung(abt_id),
 );
 
---select * from mitarbeiter;
+SELECT * FROM Mitarbeiter;
+
+
