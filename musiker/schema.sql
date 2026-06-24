3eb686753a90d6895cb8bf30720e059ace36c1ad
KiraL <rekiraly@gmail.com>
2026-06-24 13:46:03 +0200

    Daten eintragen

--- musiker/schema.sql
+++ musiker/schema.sql
@@ -1,3 +1,9 @@
+ --DROP TABLE genre;
+ --DROP TABLE interpret;
+ --DROP TABLE titel;
+ --DROP TABLE veranstaltungsort;
+ --DROP TABLE in_ver;
+
 CREATE TABLE genre (
 g_id INT IDENTITY(1,1) PRIMARY KEY,
 genre VARCHAR(30)
@@ -16,3 +22,19 @@ genre_id INT,
 FOREIGN KEY (interpret_id) REFERENCES interpret(i_id),
 FOREIGN KEY (genre_id) REFERENCES genre(g_id)
 );
+
+CREATE TABLE veranstaltungsort 
+(v_id INT IDENTITY(1,1) PRIMARY KEY,
+ort_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE in_ver (
+i_nr INT,
+v_nr INT NOT NULL,
+datum_von DATE,
+datum_bis DATE,
+CONSTRAINT PK_in_ver PRIMARY KEY(i_nr, datum_von, datum_bis),
+FOREIGN KEY (v_nr) REFERENCES veranstaltungsort (v_id)
+);
+
+select * from in_ver; 
